adef0f63085b110b9ab81d898e2ba781268166b7
Philippe Benoit <contact@philippe-benoit.fr>
2014-01-18 14:11:59 +0100

    Issue #1: [Cleanup] Indent, spaces and comments

--- public/js/pannel.js
+++ public/js/pannel.js
@@ -1,56 +1,38 @@
-// Execution de cette fonction lorsque le DOM sera entièrement chargé 
+/* Cette fonction s'execute lorsque le DOM est entierement charge */
 			
-			
-			
-	$(document).ready(function() {
-	
-			
-			////////////Permet envoi les messages a tout les client////////////
-			$("#img-send").click(function() { 
-				
-				if($("#input_text").val()!="")
-				{
-					$("#message ul").append("<li>("+mon_identifiant+"): "+$("#input_text").val()+"</li>");	
-					$("#message").scrollTop(100000) ;
-					
-					socket.send(JSON.stringify(
-					{
-					message: $("#input_text").val(),
-					pseudo: mon_identifiant
-					}));
-	
-				}
-		
-			});
-	
-			///////////Permet l'ouverture et la fermeture du pannel//////
-			$("#bouton-menu").click(function() { 
-			
-					if($("#cadre-menu").css("margin-Left")=="0px")
-					{
-						$("#cadre-menu").animate({ marginLeft: "175px"  }, 400 );
-						$("#notre_frame").animate({ marginLeft: "175px" }, 400 );
-						$("#notre_frame").css("width", "calc(100% - 175px)");	
-						$("#bouton-menu").html("");
-						$("#bouton-menu").css("background", "url('images/fond-demo-menu-close.jpg')  bottom no-repeat");	
-					}else{
-						
-						$("#notre_frame").animate({ marginLeft: "0"  }, 300 );
-						$("#cadre-menu").animate({ marginLeft: "0"  }, 300 );
-						setTimeout(function (){$("#notre_frame").css("width", "100%")}, 300);	
-						$("#bouton-menu").css("background", "url('images/fond-demo-menu-open.jpg')  bottom no-repeat");
-					}
-						
-			});
-			
-			$("#boutton-user").hover(function() { 
-			$("#cadre-user").animate({ marginRight: "125px"  }, 400 );
-			
-			},function() {
-			$("#cadre-user").animate({ marginRight: "0"  }, 300 );
-			
-			});
-			
-			
-			
-	}); 
+$(document).ready(function() { 
+    
+    /* Permet d'envoyer les messages a tous les clients */
+    $("#img-send").click(function() { 
+        if ($("#input_text").val() != "") {
+            $("#message ul").append("<li>(" + mon_identifiant + "): " + $("#input_text").val() + "</li>");	
+            $("#message").scrollTop(100000) ;
+            socket.send(JSON.stringify({
+                message: $("#input_text").val(),
+                pseudo: mon_identifiant
+            }));
+        }
+    });
+
+    /* Permet l'ouverture et la fermeture du pannel */
+    $("#bouton-menu").click(function() { 
+        if ($("#cadre-menu").css("margin-Left") == "0px") {
+            $("#cadre-menu").animate({ marginLeft: "175px"  }, 400 );
+            $("#notre_frame").animate({ marginLeft: "175px" }, 400 );
+            $("#notre_frame").css("width", "calc(100% - 175px)");	
+            $("#bouton-menu").html("");
+            $("#bouton-menu").css("background", "url('images/fond-demo-menu-close.jpg')  bottom no-repeat");	
+        } else {
+            $("#notre_frame").animate({ marginLeft: "0"  }, 300 );
+            $("#cadre-menu").animate({ marginLeft: "0"  }, 300 );
+            setTimeout(function (){$("#notre_frame").css("width", "100%")}, 300);	
+            $("#bouton-menu").css("background", "url('images/fond-demo-menu-open.jpg')  bottom no-repeat");
+        }
+    });
+    
+    $("#boutton-user").hover(function() { 
+        $("#cadre-user").animate({ marginRight: "125px"  }, 400 );
+    }, function() {
+        $("#cadre-user").animate({ marginRight: "0"  }, 300 );
+    });
+}); 
--- server.js
+++ server.js
@@ -1,142 +1,131 @@
 
- //-----Chargement de express et de socket.io 
 var io = require('socket.io');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var socket = io.listen(server);
 
- //-----changement du serveur et définition du dossier statics à inclure dans le serveur
-app.configure(function(){
+ // Chargement du serveur et definition du dossier static a inclure dans le serveur
+app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
-app.get('/', function(req, res, next){
+app.get('/', function(req, res, next) {
   res.render('./public/index.html');
 });
-server.listen(8333); // Définition du port d'écoute
+server.listen(8333);
  
  
-//----------Attribut-------------
+// Attributs
 var my_timer;
 var allClients = 0;
 var tab_client = new Array();
 var slide_currently;
 var TempoPPT;
-var master=false;
+var master = false;
 
 
-//---------on définie le fichier client-----
-app.get('/', function (req, res)
-{
+// On definie le fichier client
+app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
 });
 
 
-//--------Connection d'un client-------------
-socket.on('connection', function (client)
-{
+// Connection d'un client
+socket.on('connection', function (client) {
 	allClients ++;
 	var TempoPseudo;
 	var TempoMaster;
 	
-	//--------Après avoir saisie son speudo on ouvre la session-------------
+	// Apres avoir saisie son speudo on ouvre la session
 	client.on('ouvertureSession', function (connect) {
 	
 		console.log("Ouverture Session");
 
 		var obj_connect = JSON.parse(connect);
-		console.log("LE ppt : "+obj_connect.ppt);
-		if(obj_connect.master && master==false) // on vérifie si un master existe déjà sinon il n'existe pas on lui attribue le droit
-		{
-			master=true;
-			TempoMaster=obj_connect.master;
-			TempoPseudo=obj_connect.identifant+" [master]";
-			if(obj_connect.ppt)
-				TempoPPT=obj_connect.ppt;
-		}else
-			TempoPseudo=obj_connect.identifant;
+		console.log("LE ppt : " + obj_connect.ppt);
+        
+        // On verifie si un master existe deja sinon, comme il n'existe pas, on lui attribue le droit
+		if (obj_connect.master && master == false) {
+			master = true;
+			TempoMaster = obj_connect.master;
+			TempoPseudo = obj_connect.identifant + " [master]";
+			if (obj_connect.ppt) { 
+				TempoPPT = obj_connect.ppt;
+            }
+		} else {
+			TempoPseudo = obj_connect.identifant;
+        }
 			
-		tab_client.push(TempoPseudo); // on rajoute le speudo dans la tab client
+		tab_client.push(TempoPseudo); 
 		
-
-	
-		
-		 //--------on envoi la nouvelle tab de client a l'utilisateur qui demande la connection------------- 
+        // On envoi la nouvelle tab de client a l'utilisateur qui demande la connection
 		client.send(JSON.stringify({
-		"clients": allClients,
-		"tab_client": tab_client,
-		"connexion":TempoPseudo,
-		le_slide : slide_currently,
-		ppt: TempoPPT,
-		master: TempoMaster
+            "clients": allClients,
+            "tab_client": tab_client,
+            "connexion":TempoPseudo,
+            le_slide : slide_currently,
+            ppt: TempoPPT,
+            master: TempoMaster
 		}));
-		//--------on envoi la nouvelle tab de client à tous les clients connectés------------- 
+        
+		// On envoi la nouvelle tab de client a tous les clients connectes
 		client.broadcast.send(JSON.stringify({
-		"clients": allClients,
-		"tab_client": tab_client,
-		"pseudo":TempoPseudo
+            "clients": allClients,
+            "tab_client": tab_client,
+            "pseudo": TempoPseudo
 		}));
 		 
     });
 
 	
-	//-----------reception d'un message pour la gestion des slide et l'envoi au poste esclave-----------------
-	client.on('message', function (data)
-	{
+	// Reception d'un message pour la gestion des slide et l'envoi au poste esclave
+	client.on('message', function (data) {
 		var obj_client = JSON.parse(data);
-		
-		slide_currently=obj_client.slide;
+		slide_currently = obj_client.slide;
 		
 		client.broadcast.send(JSON.stringify({
-      
 			le_next: obj_client.suivant,
 			le_prev: obj_client.precedant,
 			le_first: obj_client.premier,
 			le_last: obj_client.dernier,
-			le_msg: obj_client.message, //channel de discution
-			le_pseudo: obj_client.pseudo, //speudo
-			le_slide: obj_client.slide,   // id du SLide
+			le_msg: obj_client.message,      // channel de discution
+			le_pseudo: obj_client.pseudo,    // pseudo
+			le_slide: obj_client.slide,      // id du SLide
 			url: obj_client.url
-		}));
-    
+		}));  
 	});
 	
-	//---------Réception de l'id de l'élément cliqué et on envoi l'id à tous les clients ------
+	// Reception de l'id de l'element clique et on envoi l'id a tous les clients
 	client.on('envoiRefObjetHtml', function (idtempo) {
-
 		client.broadcast.emit('recupObjetHtml', idtempo);
-
     });
 	
-	//-------------Réception d'un traitement video et l'envoi à tout les clients --------------
+	// Reception d'un traitement video et l'envoi a tous les clients
 	client.on('envoiControlVideo', function (video) {
-		
 		var obj_video = JSON.parse(video);
 		client.broadcast.emit('emettreControlVideo', JSON.stringify({
-      
 			pause: obj_video.pause,
 			play: obj_video.play, 
-			toPlay: obj_video.toPlay //jouer la video sur position de lecture saisie
+			toPlay: obj_video.toPlay     // jouer la video sur position de lecture saisie
 		}));
-
     });
 
 
-	//------lors de la déconnexion d'un client ------------------------
-	client.on('disconnect', function ()
-	{
-	
-		console.log('disconnect '+TempoPseudo);
+	// Lors de la deconnexion d'un client
+	client.on('disconnect', function () {
+		console.log('disconnect ' + TempoPseudo);
 		
-		if(TempoPseudo)
-		{
-			tab_client.splice(tab_client.indexOf(TempoPseudo),1); // on retire de la table le pseudo du client qui s'est déconnecté
-			if(TempoPseudo.indexOf("[master]")!=-1)  // on libère le jeton du master
-				master=false;
+		if (TempoPseudo) {
+			tab_client.splice(tab_client.indexOf(TempoPseudo), 1);
+			if (TempoPseudo.indexOf("[master]") != -1) {
+				master = false;
+            }
 		}
+        
 		allClients -= 1;
-		client.broadcast.send(JSON.stringify( //on renvoi la nouvelle table de client à tous les clients
-		{
+        
+        // On renvoi la nouvelle table de client a tous les clients
+		client.broadcast.send(JSON.stringify({
 		  "clients": allClients,
 		  "tab_client": tab_client,
 		  "deconnexion": TempoPseudo
