e5ed5a6a68866a2e0b33b02b5cfd8731f1eabe34
Philippe Benoit <contact@philippe-benoit.fr>
2014-04-06 20:18:05 +0200

    [Cleanup] Remove lot of unused code and a little refactoring in login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,66 +1,34 @@
 /* Globals variables */
-var TempoMaster = false;
 var master = false;
-var mon_identifiant;
+var identifiant;
 var password;
 var socket;
-var slideControlContainer;
-var containers;
-var currentSlide = 0;
-var tab_windows_opened = []; // this tab contains all pseudos of all opened windows 
-var messages_history = []; // This tab contains history of all opened windows. 
-var presentationsList = [];
 var token;
 
 $(document).ready(function () {
     "use strict";
 
-    var token, socket;
-    
-    $("#identification").click(function (e) {
-        console.log("entré dans le controlleur du boutton");
-        e.preventDefault();
-        mon_identifiant = $('#identifiant').val();
-        password = $('#password').val();
-        console.log(mon_identifiant + " " + password);
-        $.post('/login', {
-            type: 'POST',
-            data: {
-                identifiant: mon_identifiant,
-                password: password
-            }
-        }).done(function (result) {
-            connect_socket(result.token);
-        });
-    });
-
     // This event allow users' connection by simply Enter
-    $("#identifiant").keypress(function(e) {    
-        if(e.keyCode == 13) {
-         $("#identification").click();
+    $('#password').keypress(function(e) {
+        if (e.keyCode === 13) {
+            $("#identification").click();
         }
     });
     
-    function connect_socket(token) {
-        sessionStorage.setItem('token', token);
-        socket = io.connect('', {
-            query: 'token=' + token
-        });
-    
-        socket.emit('ouvertureSession', JSON.stringify({
-            identifant: mon_identifiant,
-            password: password,
-            socketId: socket.id
-        }));
-      
-        socket.on('login_success', function(data) {
+    // When the user click on the login button
+    $("#identification").click(function (e) {
+        e.preventDefault();
+        identifiant = $('#identifiant').val();
+        password = $('#password').val();
+        $.post('/login', {
+            type: 'POST',
+            data: {
+                identifiant: identifiant,
+                password: password
+            }
+        }).done(function (result) {
+            sessionStorage.setItem('token', result.token);
             document.location.href = "/index.html";
-        }).on('time', function (data) {
-            console.log('- broadcast: ' + data);
-        }).on('authenticated', function () {
-            console.log('- authenticated');
-        }).on('disconnect', function () {
-            console.log('- disconnected');
         });
-    }
+    });
 });
