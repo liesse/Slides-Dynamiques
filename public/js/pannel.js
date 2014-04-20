86b4e86213fe0592ca27e0ac892e1e89e5d23ac0
liesse <liesse.nadji@gmail.com>
2014-04-20 12:34:21 +0200

    Fixing bugs on global discussion channel

--- public/js/pannel.js
+++ public/js/pannel.js
@@ -4,30 +4,38 @@ $(document).ready(function() {
     
     /* Send messages to all clients */
     $("#img-send").click(function() { 
+
         if ($("#input_text").val() != "") {
-            $("#message ul").append("<li>(" + mon_identifiant + "): " + $("#input_text").val() + "</li>");	
-            $("#message").scrollTop(100000) ;
+            $("#message ul").append("<li style='font-weight:bold;'>" + identifiant + " : " + $("#input_text").val() + "</li>");	
+            $("#message").scrollTop(100000);
+
             socket.send(JSON.stringify({
                 messageContent: $("#input_text").val(),
-                messageSender: mon_identifiant
+                messageSender: identifiant
             }));
         }
+
     });
 
-    /* Allow to open and close the panel */
+    /* Allow to open and close the panel. The goal is to see and broadcast information to all connected users */
     $("#bouton-menu").click(function() { 
+
         if ($("#cadre-menu").css("margin-Left") == "0px") {
-            $("#cadre-menu").animate({ marginLeft: "175px"  }, 400 );
-            $("#notre_frame").animate({ marginLeft: "175px" }, 400 );
+
+            $("#cadre-menu").animate({marginLeft: "175px"}, 400 );
+            $("#notre_frame").animate({ marginLeft: "175px"}, 400 );
             $("#notre_frame").css("width", "calc(100% - 175px)");	
             $("#bouton-menu").html("");
             $("#bouton-menu").css("background", "url('images/fond-demo-menu-close.jpg')  bottom no-repeat");	
-        } else {
+        }
+        else {
+
             $("#notre_frame").animate({ marginLeft: "0"  }, 300 );
             $("#cadre-menu").animate({ marginLeft: "0"  }, 300 );
             setTimeout(function (){$("#notre_frame").css("width", "100%")}, 300);	
             $("#bouton-menu").css("background", "url('images/fond-demo-menu-open.jpg')  bottom no-repeat");
         }
+
     });
     
 }); 
